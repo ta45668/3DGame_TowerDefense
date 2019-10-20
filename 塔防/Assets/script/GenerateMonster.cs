d598633d6787aabfd94d8f94df523713eb043543
coocoeman <54761438+coocoeman@users.noreply.github.com>
2019-10-20 22:51:11 +0800

    整理C#

---
+++ 塔防/Assets/script/GenerateMonster.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GenerateMonster : MonoBehaviour
+{
+    #region 
+    [Header("產生敵人的設定")]
+    [Tooltip("敵人物件")] public Transform enemyTransform;
+    [Tooltip("敵人產生位置")] public Transform generateLocation;
+
+    [Tooltip("間格時間")] public float waveInterval = 5;
+    [Tooltip("敵人數量")] public int enemyQuantity = 0;
+    private float reciprocalTime = 5f;//暫存倒數的時間
+
+    #endregion
+
+    private void Update()
+
+    {
+        ReciprocalTime();
+    }
+
+    /// <summary>
+    /// 每一波的產生
+    /// </summary>
+    private void ReciprocalTime()
+    {
+        if (reciprocalTime<=0)
+        {
+            StartCoroutine(EceryGenerateQuantity());
+            reciprocalTime = waveInterval;
+        }
+        reciprocalTime -= Time.deltaTime;
+    }
+
+    /// <summary>
+    /// 每波敵人之間的間隔
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator EceryGenerateQuantity()
+    {
+        enemyQuantity++;
+        for (int i = 0; i < enemyQuantity; i++)
+        {
+            EnemyGenerateLocation();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    /// <summary>
+    /// 敵人生成的位置
+    /// </summary>
+    private void EnemyGenerateLocation()
+    {
+        Instantiate(enemyTransform,generateLocation.position,generateLocation.rotation);
+    }
+}
---
+++ 塔防/Assets/script/HeroAccack.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroAccack : MonoBehaviour
+{
+    public GameObject hitParticle;
+    private Transform accackTarget;
+    [Tooltip("飛行速度")] public float flightSpeed = 70f;
+    private float spreadRange;
+    private int attackValue;
+
+    /// <summary>
+    /// 等英雄的資料傳進來
+    /// </summary>
+    /// <param 鎖定目標="target"></param>
+    /// <param 波及範圍="range"></param>
+    /// <param 傷害值="accackValue"></param>
+    public void Search(Transform target, float range, int accackValue)
+    {
+        accackTarget = target;
+        spreadRange = range;
+        attackValue = accackValue;
+    }
+
+    private void Update()
+    {
+        if(accackTarget == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Flight();
+    }
+
+    /// <summary>
+    /// 攻擊飛向敵人
+    /// </summary>
+    void Flight()
+    {
+        Vector3 dir = accackTarget.position - transform.position;
+        float targetDistance = flightSpeed * Time.deltaTime;
+        if (dir.magnitude <= targetDistance)
+        {
+            HitTarget();
+            return;
+        }
+        transform.Translate(dir.normalized * targetDistance, Space.World);
+    }
+
+    /// <summary>
+    /// 擊中目標 使用特效
+    /// </summary>
+    void HitTarget()
+    {
+        GameObject useHitParticle = (GameObject)Instantiate(hitParticle, transform.position, transform.rotation);
+        Destroy(useHitParticle, 2f);
+
+        if (spreadRange>0f)
+        {
+            SpreadRange();
+        }
+        else
+        {
+            HurtEnemy();
+        }
+        Destroy(gameObject);
+    }
+
+    void HurtEnemy()
+    {
+
+    }
+
+    /// <summary>
+    /// 設定偵測範圍
+    /// </summary>
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, spreadRange);
+    }
+
+    void SpreadRange()
+    {
+
+    }
+}
